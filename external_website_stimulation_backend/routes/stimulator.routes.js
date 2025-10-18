b73ab9d818bee29954b77ee1cc3a44c51b804a0f
kashishkataria786 <offverse.business@gmail.com>
2025-10-19 01:50:22 +0600

    Initialize external website stimulation backend with Express, CORS, and Mongoose setup. Added basic server configuration and routes.

---
+++ external_website_stimulation_backend/routes/stimulator.routes.js
@@ -0,0 +1,5 @@
+import express from 'express'
+
+const stimultorRouter = express.Router();
+
+export default stimulatorRouter;
---
+++ external_website_stimulation_backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from "dotenv";
+import morgan from 'morgan'
+
+const app = express();
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+app.use(morgan('combined'));
+app.get('/', ()=>{res.send("<h1>Stimulator Website backend</h1>")})
+
+const PORT = process.env.PORT || 5001
+if(process.env.NODE_ENV !=='production'){
+    app.listen(PORT, ()=>{
+        console.log(`Server started at ${PORT}`)
+    })
+}
+export default app;
